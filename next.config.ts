2a90518285240d064d37817614eab4e8bc94c157
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2024-11-07 09:01:35 +0700

    setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "samawabedummy-main.test",
+        port: "",
+        pathname: "/api/**",
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.module?.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
